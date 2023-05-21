57fef050e2ee14aea8d083441dbdb3cb6956a20b
Kamil <kamilratajczyk00@gmail.com>
2023-05-21 20:45:28 +0200

    rest of files

--- SDL/include/SDL_revision.h
+++ SDL/include/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.26.0-355-g120bc77e9 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.27.0-no-vcs (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-2.26.0-355-g120bc77e9"
+#define SDL_REVISION "SDL-2.27.0-no-vcs"
 #endif
